e55d343d59485b73506408f16c14f74e13542fdd
skoplev <skoplev@gmail.com>
2016-11-11 10:26:31 -0500

    File support for filenames with many '.'s

--- runCibersort.r
+++ runCibersort.r
@@ -45,7 +45,7 @@ dir.create(opt$options$output)  # create output folder
 write.table(results,
 	file.path(opt$options$output,
 		paste0(
-			strsplit(basename(expr_file), "[.]")[[1]][1],  # base file name without extension
+			basename(expr_file),
 			".tsv")),
 	sep="\t",
 	quote=FALSE)
